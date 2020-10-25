8c61a9371baa0ecd9b6c74e5973a6201678fd9d8
fabsiqueira <fabssiqueira@hotmail.com>
2020-10-25 21:50:09 +0000

    securtity token

--- src/main/java/br/gov/sp/fatec/springbootapp/exception/RegistroNaoEncontradoException.java
+++ src/main/java/br/gov/sp/fatec/springbootapp/exception/RegistroNaoEncontradoException.java
@@ -28,3 +28,4 @@ public class RegistroNaoEncontradoException extends RuntimeException{
 
     }
 }
+
--- src/main/java/br/gov/sp/fatec/springbootapp/security/SecurityConfig.java
+++ src/main/java/br/gov/sp/fatec/springbootapp/security/SecurityConfig.java
@@ -1,20 +1,46 @@
 package br.gov.sp.fatec.springbootapp.security;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @EnableWebSecurity
-@EnableGlobalMethodSecurity(prePostEnabled = true)
+@EnableGlobalMethodSecurity(prePostEnabled = true) // está seguro onde tem anotações
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
-    @Override
-    protected void configure(HttpSecurity http) throws Exception{
-        http.csrf().disable().httpBasic().and()
+    @Autowired
+    private UserDetailsService userDetailsService;
 
-        .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.csrf().disable().addFilterBefore(new JwtAuthenticationFilter(), UsernamePasswordAuthenticationFilter.class)
+                // desabilita a criação no Spring security
+                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
     }
-    
+
+    @Override
+    public void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService(userDetailsService);
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoderBean() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    @Override
+    public AuthenticationManager authenticationManagerBean() throws Exception {
+        return super.authenticationManagerBean();
+    }
 }
