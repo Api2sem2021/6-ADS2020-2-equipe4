cb002d9b8b9e30761a14ab06121858ec9444bfd9
Arthur Mergulhão <alves.m.arthur@gmail.com>
2020-11-28 11:22:35 -0300

    Alterada a estrutura do roteamento do painel

---
+++ client/src/dist/routes.dev.js
@@ -0,0 +1,65 @@
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports["default"] = void 0;
+
+var _vueRouter = require("vue-router");
+
+var _Login = _interopRequireDefault(require("./components/Login.vue"));
+
+var _Panel = _interopRequireDefault(require("./components/Panel.vue"));
+
+var _Notifications = _interopRequireDefault(require("./components/Notifications.vue"));
+
+var _Chat = _interopRequireDefault(require("./components/Chat.vue"));
+
+var _Employes = _interopRequireDefault(require("./components/Employes.vue"));
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { "default": obj }; }
+
+var routes = [{
+  path: '/login',
+  name: 'Login',
+  component: _Login["default"]
+}, {
+  path: '/panel',
+  name: 'Panel',
+  component: _Panel["default"],
+  children: [{
+    path: '/panel/activities',
+    name: 'Notifications',
+    component: _Notifications["default"]
+  }, {
+    path: '/panel/live_chats',
+    name: 'Chat',
+    component: _Chat["default"]
+  }, {
+    path: '/panel/employes',
+    name: 'Employes',
+    component: _Employes["default"]
+  }]
+}, {
+  path: '/',
+  redirect: {
+    path: '/panel/activities'
+  }
+}];
+var router = (0, _vueRouter.createRouter)({
+  history: (0, _vueRouter.createWebHistory)(),
+  routes: routes
+});
+router.beforeEach(function (to, from, next) {
+  if (to.name !== 'Login' && !sessionStorage.usuario) {
+    next({
+      path: '/login'
+    });
+  } else if (to.name == 'Login' && sessionStorage.usuario) {
+    next({
+      path: '/panel/activities'
+    });
+  } else next();
+});
+var _default = router;
+exports["default"] = _default;
--- client/src/routes.js
+++ client/src/routes.js
@@ -1,35 +1,55 @@
-import {createWebHistory, createRouter} from 'vue-router';
+import { createWebHistory, createRouter } from 'vue-router';
 import Login from './components/Login.vue';
 import Panel from './components/Panel.vue';
+import Notifications from './components/Notifications.vue';
+import Chat from './components/Chat.vue';
+import Employes from './components/Employes.vue';
 
 const routes = [
     {
         path: '/login',
         name: 'Login',
-        component:Login
+        component: Login
     },
     {
         path: '/panel',
         name: 'Panel',
-        component:Panel
+        component: Panel,
+        children: [
+            {
+                path: '/panel/activities',
+                name: 'Notifications',
+                component: Notifications
+            },
+            {
+                path: '/panel/live_chats',
+                name: 'Chat',
+                component: Chat
+            },
+            {
+                path: '/panel/employes',
+                name: 'Employes',
+                component: Employes
+            }
+        ]
     },
     {
-        path: '/', redirect: { path: '/panel?url=notifications' }
+        path: '/', redirect: { path: '/panel/activities' }
     }
-    
+
 ];
 
 const router = createRouter({
-    history:createWebHistory(),
+    history: createWebHistory(),
     routes
 });
 
 router.beforeEach((to, from, next) => {
-    if (to.name !== 'Login' && !sessionStorage.usuario){ next({ path: '/login' })}
+    if (to.name !== 'Login' && !sessionStorage.usuario) { next({ path: '/login' }) }
 
-    else if (to.name == 'Login' && sessionStorage.usuario){ next({ path: '/panel?url=notifications' })}
-    
+    else if (to.name == 'Login' && sessionStorage.usuario) { next({ path: '/panel/activities' }) }
+
     else next();
-  })
+})
 
 export default router;
